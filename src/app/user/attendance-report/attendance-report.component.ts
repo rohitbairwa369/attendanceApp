d2ea1feb9be26b9aee8fe5ccdd34ef9c8a53ce4a
rohitbairwa369 <rohitbairwa36910@gmail.com>
2024-02-10 16:53:25 +0530

    report card change

--- src/app/user/attendance-report/attendance-report.component.ts
+++ src/app/user/attendance-report/attendance-report.component.ts
@@ -3,16 +3,18 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MekaService } from '../../service/meka.service';
 import { MtableComponent } from '../../mtable/mtable.component';
 import { NotificationService } from '../../service/notification.service';
-
+import { CalendarModule } from 'primeng/calendar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-attendance-report',
   standalone: true,
-  imports: [ReactiveFormsModule,MtableComponent],
+  imports: [ReactiveFormsModule,MtableComponent,CalendarModule,ButtonModule],
   templateUrl: './attendance-report.component.html',
   styleUrl: './attendance-report.component.css'
 })
 export class AttendanceReportComponent {
+  loading:boolean = false;
   absentData:any =[];
   months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
   token = JSON.parse(localStorage.getItem('token'));
@@ -25,13 +27,16 @@ export class AttendanceReportComponent {
 
   onSubmit(){
     if(this.reportForm.valid){
+      this.loading = true;
+      const dateyear = this.reportForm.value.monthYear.toString()
     const data = {
-      month : this.months[this.reportForm.value.monthYear.split('-')[1]-1],
-      year : this.reportForm.value.monthYear.split('-')[0]
+      month : dateyear.split(' ')[1],    //   month : this.months[this.reportForm.value.monthYear.split('-')[1]-1],
+      year : dateyear.split(' ')[3]
     }
     this.selectedMonth = data.month;
     this.mekaService.getAbsentDates(data,this.token).subscribe(res=>{
     this.absentData = res;
+    this.loading = false;
     })
     }else{
       this.notify.notify({ severity: 'info', summary: 'Invalid Form', detail: 'Response is empty or invalid', life: 3000 })
