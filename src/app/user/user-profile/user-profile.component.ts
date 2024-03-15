06fe9dd95a9449b39ae1277722aeb04237ce1be5
shrutika <shrutika.s.w123@gmail.com>
2024-03-15 06:49:33 -0700

    profile html ts file

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -2,39 +2,63 @@ import { Component } from '@angular/core';
 import { MekaService } from '../../service/meka.service';
 import { CommonModule } from '@angular/common';
 import { SkeletonModule } from 'primeng/skeleton';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators,FormsModule } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { InputTextModule } from 'primeng/inputtext';
 import { takeUntil } from 'rxjs';
 import { unsub } from '../../shared/unsub.class';
 import { NotificationService } from '../../service/notification.service';
+import { CalendarModule } from 'primeng/calendar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule,SkeletonModule,ReactiveFormsModule,InputTextModule],
+  imports: [CommonModule,SkeletonModule,ReactiveFormsModule,InputTextModule,CalendarModule,RadioButtonModule,FormsModule,InputTextareaModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent extends unsub {
   userData: any={};
-  profileForm:FormGroup;
+  profileForm!:FormGroup;
   imageUrl: SafeResourceUrl;
   selectedFile: any;
   token = JSON.parse(localStorage.getItem('token'));
-  constructor(private mekaService:MekaService,private sanitizer: DomSanitizer,private notify : NotificationService){
+  constructor(private mekaService:MekaService,private sanitizer: DomSanitizer,private notify : NotificationService, private FormBuilder: FormBuilder){
     super()
     this.mekaService.myUserData$.pipe(takeUntil(this.onDestroyed$)).subscribe(data=>{
       this.userData = data;
       this.imageUrl = this.userData.profilePic;
-      this.profileForm=new FormGroup({
-        'name':new FormControl(this.userData.name,Validators.required),
-        'email': new FormControl(this.userData.email,Validators.required),
-        'password':new FormControl(this.userData.password,Validators.required),
-        'mobile_number':new FormControl(this.userData.mobile,[Validators.required]),
-        'location':new FormControl(this.userData.location,Validators.required),
-        'pin':new FormControl(this.userData.pin,Validators.required),
-        'company':new FormControl(this.userData.company_name,Validators.required)
-    
+      
+      this.profileForm=this.FormBuilder.group({
+        // 'name':new FormControl(this.userData.name,Validators.required),
+        // 'email': new FormControl(this.userData.email,Validators.required),
+        // 'teamname':new FormControl(this.userData.teamname,Validators.required),
+        // 'DOB':new FormControl(this.userData.DOB,[Validators.required]),
+        // 'Gender':new FormControl(this.userData.Gender,Validators.required),
+        // 'role':new FormControl(this.userData.role,Validators.required),
+        // 'contact':new FormGroup(this.userData.contact,Validators.required),
+        // 'company':new FormControl(this.userData.company_name,Validators.required),
+        // 'reportingto':new FormControl(this.userData.reportingto,Validators.required),
+        // 'AddressLine':new FormControl(this.userData.AddressLine,Validators.required),
+
+        name: [this.userData.name, [Validators.required]],
+        email: [this.userData.email, [Validators.required, Validators.email]],
+        gender: [this.userData.gender, [Validators.required]],
+        role: [this.userData.role, [Validators.required]],
+        contact:[
+          this.userData.contact,
+          [
+            Validators.required,
+            Validators.pattern('^[0-9]{2,30}$'),
+            Validators.minLength(10),
+            Validators.maxLength(10)
+          ],
+        ],
+        birthDate: [this.userData.birthDate, [Validators.required]],
+        reportingTo: [this.userData.reportingTo, [Validators.required]],
+        teamCategory: [this.userData.teamCategory, [Validators.required]],
+        address: [this.userData.address, [Validators.required]],   
       })
     })
   }
