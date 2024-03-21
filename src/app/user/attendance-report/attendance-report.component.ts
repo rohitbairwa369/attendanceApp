e00bbcd6b8206e714f36170cab88b07c802ef35c
rohitbairwa369 <rohitbairwa36910@gmail.com>
2024-03-21 23:25:37 +0530

    new way to view report added

--- src/app/user/attendance-report/attendance-report.component.ts
+++ src/app/user/attendance-report/attendance-report.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MekaService } from '../../service/meka.service';
 import { MtableComponent } from '../../mtable/mtable.component';
 import { NotificationService } from '../../service/notification.service';
 import { CalendarModule } from 'primeng/calendar';
 import { ButtonModule } from 'primeng/button';
 import { Title } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-attendance-report',
   standalone: true,
-  imports: [ReactiveFormsModule,MtableComponent,CalendarModule,ButtonModule],
+  imports: [CommonModule,ReactiveFormsModule,MtableComponent,CalendarModule,ButtonModule,FormsModule],
   templateUrl: './attendance-report.component.html',
   styleUrl: './attendance-report.component.css'
 })
@@ -22,7 +23,9 @@ export class AttendanceReportComponent {
   reportForm = new FormGroup({
     'monthYear': new FormControl(null,[Validators.required]),
   })
-  selectedMonth:string='none'
+  onlyAbsentDate=[]
+  todaysDate=new Date();
+  selectedMonth:string='Please select a month!'
 
   constructor(private titleService: Title,private mekaService:MekaService,private notify:NotificationService){
     this.titleService.setTitle("Meka - Report")
@@ -31,6 +34,7 @@ export class AttendanceReportComponent {
   onSubmit(){
     if(this.reportForm.valid){
       this.loading = true;
+      this.onlyAbsentDate=[]
       const dateyear = this.reportForm.value.monthYear.toString()
     const data = {
       month : dateyear.split(' ')[1],    //   month : this.months[this.reportForm.value.monthYear.split('-')[1]-1],
@@ -39,6 +43,15 @@ export class AttendanceReportComponent {
     this.selectedMonth = data.month;
     this.mekaService.getAbsentDates(data,this.token).subscribe(res=>{
     this.absentData = res;
+    if(this.absentData && this.absentData.length>0){
+    var newDate =`${this.absentData[0].date}/${this.absentData[0].month}/${this.absentData[0].year}`
+    this.todaysDate= new Date(newDate)
+    }else{
+      this.todaysDate=new Date()
+    }
+    this.absentData.forEach(element => {
+      this.onlyAbsentDate.push(parseInt(element.date))
+    });
     this.loading = false;
     })
     }else{
