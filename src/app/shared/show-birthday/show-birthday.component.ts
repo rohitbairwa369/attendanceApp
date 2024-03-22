1dca3b8917fd8d96310a62a816d8ebbafe4b1444
shrutika <shrutika.s.w123@gmail.com>
2024-03-22 04:27:14 -0700

    birthday slider

--- src/app/shared/show-birthday/show-birthday.component.ts
+++ src/app/shared/show-birthday/show-birthday.component.ts
@@ -11,15 +11,57 @@ import { DatePipe } from '@angular/common';
 })
 export class ShowBirthdayComponent implements OnInit{
 
-mekaService = inject(MekaService)
+  token = JSON.parse(localStorage.getItem('token'))
+
+  months = [
+    "Jan", // January
+    "Feb", // February
+    "Mar", // March
+    "Apr", // April
+    "May", // May
+    "Jun", // June
+    "Jul", // July
+    "Aug", // August
+    "Sep", // September
+    "Oct", // October
+    "Nov", // November
+    "Dec"  // December
+  ];
+  
+  selectedMonth:any;
+  mekaService = inject(MekaService)
   usersbirthday: any;
 ngOnInit(): void {
   const token = JSON.parse(localStorage.getItem('token'))
     var todaysDate = new Date();
+    this.selectedMonth = todaysDate.getMonth()
     this.mekaService.getWhoseBirthday((todaysDate.getMonth()+1),token).subscribe(res=>{
       this.usersbirthday = res;
     },error=>{
 
     })
+}
+
+previousMonthe(){
+
+if(this.selectedMonth>=1){
+this.selectedMonth--;
+this.mekaService.getWhoseBirthday((this.selectedMonth+1),this.token).subscribe(res =>{
+  this.usersbirthday = res;
+})
+}else{
+  this.selectedMonth =11
+}
+}
+
+nextMonth(){
+  if(this.selectedMonth<=10){
+  this.selectedMonth ++;
+  this.mekaService.getWhoseBirthday((this.selectedMonth+1),this.token).subscribe(res =>{
+  this.usersbirthday = res;
+})
+  }else{
+    this.selectedMonth = 0;
+  }
 }
 }
